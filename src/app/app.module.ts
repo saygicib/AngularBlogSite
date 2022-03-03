ddd1d664ff084bf5911b439c1b394abd326df2bd
saygicib <bsaygici@hotmail.com>
2022-03-03 17:32:02 +0300

    Added necessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutmeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
